4384574360c4822e0912437e3568577461d86dbf
karyanayandi <halo@yandi.me>
2023-06-19 12:13:12 +0700

    fix: use client directive

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-  clean: false,
+  minify: false,
+  sourcemap: true,
   dts: true,
   entry: ["src/index.ts"],
-  splitting: false,
-  format: ["cjs", "esm"],
-  sourcemap: false,
-  target: "esnext",
-  outDir: "dist",
+  clean: true,
+  target: "es2020",
+  external: ["react"],
+  format: ["esm", "cjs"],
 })
